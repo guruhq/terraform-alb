4b4744cc8c0c7f693964b5d3ff902aa1611f1157
Karl Girthofer <karlgirthofer@gmail.com>
2018-05-31 10:47:32 -0400

    adding ssl

--- alb.tf
+++ alb.tf
@@ -49,3 +49,16 @@ resource "aws_lb_listener" "http" {
     type             = "forward"
   }
 }
+
+resource "aws_lb_listener" "front_end" {
+  load_balancer_arn = "${aws_lb.main.arn}"
+  port              = "443"
+  protocol          = "HTTPS"
+  ssl_policy        = "ELBSecurityPolicy-TLS-1-1-2017-01"
+  certificate_arn   = "arn:aws:acm:us-west-2:495243515911:certificate/a7b25b53-cf9c-4e16-8e25-d6dea0a78f23"
+
+  default_action {
+    target_group_arn = "${aws_lb_target_group.main.arn}"
+    type             = "forward"
+  }
+}
