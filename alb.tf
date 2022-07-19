eb25fc8d38caa67d07221411bb4268543e71a4e8
Shawn Catanzarite <me@shawncatz.com>
2022-07-19 14:40:56 -0700

    ignore changes on listeners and remove unnecessary string interpolation

--- alb.tf
+++ alb.tf
@@ -2,46 +2,46 @@ resource "aws_lb" "main" {
   security_groups    = var.lb_security_groups
   subnets            = var.public_subnet_ids
   name               = "${var.project}-${var.environment}"
-  internal           = "${var.lb_internal_bool}"
+  internal           = var.lb_internal_bool
   load_balancer_type = "application"
 
   access_logs {
-    bucket  = "${var.access_logs_bucket}"
+    bucket  = var.access_logs_bucket
     prefix  = "${var.project}-${var.environment}"
-    enabled = "${var.access_logs_enabled}" 
+    enabled = var.access_logs_enabled
   }
 
   tags = {
     Name        = "${var.project}-${var.environment}"
-    Project     = "${var.project}"
-    Environment = "${var.environment}"
+    Project     = var.project
+    Environment = var.environment
   }
 }
 
 resource "aws_lb_target_group" "main" {
-  name        = "${var.project}-${var.environment}-tg"
+  name = "${var.project}-${var.environment}-tg"
 
-  port        = "${var.port}"
+  port        = var.port
   protocol    = "HTTP"
-  vpc_id      = "${var.vpc_id}"
-  target_type = "${var.target_type}"
+  vpc_id      = var.vpc_id
+  target_type = var.target_type
 
-  deregistration_delay = "${var.deregistration_delay}"
-  slow_start           = "${var.slow_start}"
+  deregistration_delay = var.deregistration_delay
+  slow_start           = var.slow_start
 
   health_check {
-    healthy_threshold   = "${var.healthy_threshold}"
-    interval            = "${var.interval}"
+    healthy_threshold   = var.healthy_threshold
+    interval            = var.interval
     protocol            = "HTTP"
-    timeout             = "${var.timeout}"
-    path                = "${var.health_check_path}"
-    unhealthy_threshold = "${var.unhealthy_threshold}"
-    matcher             = "${var.hc_matchers}"
+    timeout             = var.timeout
+    path                = var.health_check_path
+    unhealthy_threshold = var.unhealthy_threshold
+    matcher             = var.hc_matchers
   }
   tags = {
     Name        = "${var.project}-${var.environment}-tg"
-    Project     = "${var.project}"
-    Environment = "${var.environment}"
+    Project     = var.project
+    Environment = var.environment
   }
   depends_on = [
     "aws_lb.main"
@@ -49,25 +49,33 @@ resource "aws_lb_target_group" "main" {
 }
 
 resource "aws_lb_listener" "http" {
-  load_balancer_arn = "${aws_lb.main.id}"
-  port              = "${var.lb_port}"
+  load_balancer_arn = aws_lb.main.id
+  port              = var.lb_port
   protocol          = "HTTP"
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.main.id}"
+    target_group_arn = aws_lb_target_group.main.id
     type             = "forward"
+  }
+
+  lifecycle {
+    ignore_changes = [default_action[0]]
   }
 }
 
 resource "aws_lb_listener" "https" {
-  load_balancer_arn = "${aws_lb.main.arn}"
+  load_balancer_arn = aws_lb.main.arn
   port              = "443"
   protocol          = "HTTPS"
-  ssl_policy        = "${var.ssl_policy}"
-  certificate_arn   = "${var.certificate_arn}"
+  ssl_policy        = var.ssl_policy
+  certificate_arn   = var.certificate_arn
 
   default_action {
-    target_group_arn = "${aws_lb_target_group.main.arn}"
+    target_group_arn = aws_lb_target_group.main.arn
     type             = "forward"
+  }
+
+  lifecycle {
+    ignore_changes = [default_action[0]]
   }
 }
